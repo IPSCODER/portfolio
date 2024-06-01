34817e9dfe34916763f8fb3af8d0e76e61dc963d
ipscoder <psawant0015@gmail.com>
2024-06-01 08:09:39 +0530

    first

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
+import AboutSection from "@/components/homepage/about";
+import Experience from "@/components/homepage/experience";
 import HeroSection from "@/components/homepage/hero-section";
+import Projects from "@/components/homepage/projects";
+import Skills from "@/components/homepage/skills";
 
 export default function Home() {
   return (
     <>
     <HeroSection/>
+    <AboutSection/>
+    <Experience/>
+    <Skills/>
+    <Projects/>
     </>
   );
 }
